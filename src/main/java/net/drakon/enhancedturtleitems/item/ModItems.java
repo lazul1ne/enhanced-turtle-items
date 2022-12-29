65f740835e36226c39862bb44b55a4b96bbe52a2
AbyssDweller <80689879+AbyssDweller@users.noreply.github.com>
2022-12-29 14:10:29 -0500

    added pickaxes

--- src/main/java/net/drakon/enhancedturtleitems/item/ModItems.java
+++ src/main/java/net/drakon/enhancedturtleitems/item/ModItems.java
@@ -2,6 +2,7 @@ package net.drakon.enhancedturtleitems.item;
 
 import net.drakon.enhancedturtleitems.EtiMod;
 import net.drakon.enhancedturtleitems.item.custom.ModArmorItem;
+import net.drakon.enhancedturtleitems.item.custom.ModPickaxeItem;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.entity.EquipmentSlot;
 
@@ -21,7 +22,7 @@ import net.minecraft.util.registry.Registry;
 public class ModItems {
 
 
-
+// helmets
 
     public static final Item TURTLE_LEATHER_HELMET = registerItem("turtle_leather_helmet",
         new ArmorItem(ModArmorMaterials.TURTLE_LEATHER_HELMET, EquipmentSlot.HEAD,
@@ -47,12 +48,12 @@ public class ModItems {
     public static final Item TURTLE_NETHERITE_HELMET = registerItem("turtle_netherite_helmet",
         new ArmorItem(ModArmorMaterials.TURTLE_NETHERITE_HELMET, EquipmentSlot.HEAD,
             new FabricItemSettings().group(ModItemGroup.ETIGROUP)));
-
+// misc
     public static final Item TEST = registerItem("test",
             new Item(new FabricItemSettings().group(ItemGroup.MISC)));
     public static final Item TURTLE_INGOT = registerItem("turtle_ingot",
         new Item(new FabricItemSettings().group(ModItemGroup.ETIGROUP)));
-
+// swords
     public static final Item TURTLE_GOLD_SWORD = registerItem("turtle_gold_sword",
         new SwordItem(ModToolMaterials.TURTLE_GOLD, 0, -2.25f,
             new FabricItemSettings().group(ModItemGroup.ETIGROUP)));
@@ -67,6 +68,22 @@ public class ModItems {
 
     public static final Item TURTLE_NETHERITE_SWORD = registerItem("turtle_netherite_sword",
         new SwordItem(ModToolMaterials.TURTLE_NETHERITE, 0, -2.25f,
+            new FabricItemSettings().group(ModItemGroup.ETIGROUP)));
+// pickaxes
+    public static final Item TURTLE_GOLD_PICKAXE = registerItem("turtle_gold_pickaxe",
+        new ModPickaxeItem(ModToolMaterials.TURTLE_GOLD, -3, -2.8f,
+            new FabricItemSettings().group(ModItemGroup.ETIGROUP)));
+
+    public static final Item TURTLE_IRON_PICKAXE = registerItem("turtle_iron_pickaxe",
+        new ModPickaxeItem(ModToolMaterials.TURTLE_IRON, -2, -2.8f,
+            new FabricItemSettings().group(ModItemGroup.ETIGROUP)));
+
+    public static final Item TURTLE_DIAMOND_PICKAXE = registerItem("turtle_diamond_pickaxe",
+        new ModPickaxeItem(ModToolMaterials.TURTLE_DIAMOND, -2, -2.8f,
+            new FabricItemSettings().group(ModItemGroup.ETIGROUP)));
+
+    public static final Item TURTLE_NETHERITE_PICKAXE = registerItem("turtle_netherite_pickaxe",
+        new ModPickaxeItem(ModToolMaterials.TURTLE_NETHERITE, -2, -2.8f,
             new FabricItemSettings().group(ModItemGroup.ETIGROUP)));
 
 
--- src/main/java/net/drakon/enhancedturtleitems/item/ModToolMaterials.java
+++ src/main/java/net/drakon/enhancedturtleitems/item/ModToolMaterials.java
@@ -8,16 +8,32 @@ import net.minecraft.util.Lazy;
 import java.util.function.Supplier;
 
 public enum ModToolMaterials implements ToolMaterial {
-  TURTLE_GOLD(MiningLevels.STONE, 132, 10.0f, 3.5f, 36, () -> {
+  TURTLE_GOLD(MiningLevels.STONE, 132, 1.5f, 3.5f, 22, () -> {
         return Ingredient.ofItems(ModItems.TEST);
-    }),  TURTLE_IRON(MiningLevels.IRON, 350, 10.0f, 5.5f, 25, () -> {
+    }),  TURTLE_IRON(MiningLevels.IRON, 350, 2.0f, 5.5f, 14, () -> {
         return Ingredient.ofItems(ModItems.TEST);
-    }),  TURTLE_DIAMOND(MiningLevels.DIAMOND, 1661, 10.0f, 6.5f, 27, () -> {
+    }),  TURTLE_DIAMOND(MiningLevels.DIAMOND, 1661, 8.0f, 6.5f, 10, () -> {
         return Ingredient.ofItems(ModItems.TEST);
-    }),  TURTLE_NETHERITE(MiningLevels.NETHERITE, 2131, 10.0f, 7.5f, 30, () -> {
+    }),  TURTLE_NETHERITE(MiningLevels.NETHERITE, 2131, 9.0f, 7.5f, 15, () -> {
         return Ingredient.ofItems(ModItems.TEST);
 });
 
+/*
+storing values for future uses
+3.5
+5.5
+6.5
+7.5
+ */
+
+    /* Vanilla Settings
+  WOOD(0, 59, 2.0f, 0.0f, 15, () -> Ingredient.of(ItemTags.PLANKS)),
+  STONE(1, 131, 4.0f, 1.0f, 5, () -> Ingredient.of(ItemTags.STONE_TOOL_MATERIALS)),
+  IRON(2, 250, 6.0f, 2.0f, 14, () -> Ingredient.of(Items.IRON_INGOT)),
+  DIAMOND(3, 1561, 8.0f, 3.0f, 10, () -> Ingredient.of(Items.DIAMOND)),
+  GOLD(0, 32, 12.0f, 0.0f, 22, () -> Ingredient.of(Items.GOLD_INGOT)),
+  NETHERITE(4, 2031, 9.0f, 4.0f, 15, () -> Ingredient.of(Items.NETHERITE_INGOT));
+   */
     private final int miningLevel;
     private final int itemDurability;
     private final float miningSpeed;
@@ -33,6 +49,7 @@ public enum ModToolMaterials implements ToolMaterial {
         this.enchantability = enchantability;
         this.repairIngredient = new Lazy<Ingredient>(repairIngredient);
     }
+
 
     @Override
     public int getDurability() {
